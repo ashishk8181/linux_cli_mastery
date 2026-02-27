d12e1ad676d9bd60e3dfcdbbf1a2f00f2edb2c7f
Ashish Kumar <ashishk8181@gmail.com>
2026-02-27 17:15:38 +0530

    updated terminal css and Ux

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './users/templates/**/*.html',
+    './templates/**/*.html',
     './users/**/*.py',
   ],
   theme: {
