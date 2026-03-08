3ba85f5b8c7ed4aadc534be1b4278cb2d0bdce11
Ashish Kumar <ashishk8181@gmail.com>
2026-03-08 18:14:17 +0530

    disable migrations for test

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 # Run migrations
-python manage.py migrate --noinput
+# python manage.py migrate --noinput
 
 # Start server (Daphne is auto-used via ASGI_APPLICATION setting)
 daphne -b 0.0.0.0 -p 8000 linux_cli_mastery.asgi:application
